3d34047d40d446cc3f1c9b8852a973068979c071
almirh <24458972+almirhodzic@users.noreply.github.com>
2024-02-24 03:29:24 +0100

    Fixed Register Errors

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { appSecurity } from '../../environments/environment';
+import { LoaderService } from 'src/app/services/loader.service';
 
 @Component({
   selector: 'app-register',
@@ -17,7 +18,8 @@ export class RegisterComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private loaderService: LoaderService
   ) { }
 
   ngOnInit() {
@@ -34,20 +36,32 @@ export class RegisterComponent implements OnInit {
   f3E: string = '';
   p1E: string = '';
 
+  handleErrors(errors: any) {
+    this.clearErrorFields();
+    if (errors.first_name && errors.first_name.length > 0) { this.f1E = errors.first_name[0]; }
+    if (errors.last_name && errors.last_name.length > 0) { this.f2E = errors.last_name[0]; }
+    if (errors.email && errors.email.length > 0) { this.f3E = errors.email[0]; }
+    if (errors.password && errors.password.length > 0) { this.p1E = errors.password[0]; }
+  }
+  
+  clearErrorFields() {
+    this.f1E = '';
+    this.f2E = '';
+    this.f3E = '';
+    this.p1E = '';
+  }
+
   submit(): void {  
+    this.loaderService.setLoading(true);
     this.authService.register(this.form.getRawValue()).subscribe({
-      next: (d) =>  { 
+      next: (d) =>  {
+        this.loaderService.setLoading(false);
         this.router.navigate(['/thankyou']);
       },
       error: (err) => { 
-      err = err.error.errors;
-      if (err.first_name && err.first_name.length > 0) { const f1E = err.first_name[0]; this.f1E = f1E; };
-      if (err.last_name && err.last_name.length > 0) { const f2E = err.last_name[0]; this.f2E = f2E; };
-      if (err.email && err.email.length > 0) { const f3E = err.email[0]; this.f3E = f3E; };
-      if (err.password && err.password.length > 0) { const p1E = err.password[0]; this.p1E = p1E; };
+        this.handleErrors(err.error.errors);
       },
       complete: () => { 
-        console.log('');
       } 
     });
   }
